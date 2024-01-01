e1a29f5d41a3ce11c5273fda02b4abca482398ca
Ibrahim Nalbant <ibrahim.nalbant@youview.com>
2024-01-01 19:29:00 +0000

    initial commit

---
+++ store/admin.py
@@ -0,0 +1,14 @@
+# Register your models here.
+
+from django.contrib import admin
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
